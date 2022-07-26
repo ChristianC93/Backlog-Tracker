2811cdd3a575b03ee21a5389fdcb8f048da12fd5
Christian Castillo <castillochristian3@gmail.com>
2022-07-26 12:27:37 -0400

    created Backlog and Completed components

--- src/components/Form.js
+++ src/components/Form.js
@@ -6,9 +6,9 @@ function Form() {
         <div>
             <h2>Add to Backlog</h2>
             <form>
-                <label>Game:</label>
+                <label> Game:</label>
                 <input type="text" placeholder="game"></input>
-                <label>Genre:</label>
+                <label> Genre:</label>
                 <input type="text" placeholder="genre"></input>
                 <input type="submit" value="Add to Backlog"></input>
             </form>
