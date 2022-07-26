dc949170ac2d9db238efbac519fd3b62cb9998e1
Christian Castillo <castillochristian3@gmail.com>
2022-07-26 11:59:53 -0400

    Created Form COmponent

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from './Header';
+import Form from './Form';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Form />
     </div>
   );
 }
---
+++ src/components/Form.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+
+function Form() {
+    return (
+        <div>
+            <h2>Add to Backlog</h2>
+            <form>
+                <label>Game:</label>
+                <input type="text" placeholder="game"></input>
+                <label>Genre:</label>
+                <input type="text" placeholder="genre"></input>
+                <input type="submit" value="Add to Backlog"></input>
+            </form>
+        </div>
+    )
+}
+
+export default Form;
