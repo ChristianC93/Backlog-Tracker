73115b3447c92d5c5befe503fff014cb675aa2d9
Christian Castillo <castillochristian3@gmail.com>
2022-07-27 10:44:42 -0400

    edited Form Component

--- src/components/Form.js
+++ src/components/Form.js
@@ -6,10 +6,10 @@ function Form() {
         <div>
             <h2>Add to Backlog</h2>
             <form>
-                <label> Game:</label>
-                <input type="text" placeholder="game"></input>
-                <label> Genre:</label>
-                <input type="text" placeholder="genre"></input>
+                <label> Name:</label><br></br>
+                <input type="text" placeholder="name..."></input><br></br>
+                <label> Genre:</label><br></br>
+                <input type="text" placeholder="genre..."></input><br></br><br></br>
                 <input type="submit" value="Add to Backlog"></input>
             </form>
         </div>
