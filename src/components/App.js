9d2a707bf8cef50b6f1200b8f43359b11e1ce73a
Christian Castillo <castillochristian3@gmail.com>
2022-07-28 17:25:13 -0400

    added data to games in db.json and created useEffect and useState

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,16 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import Header from './Header';
 import NavBar from './Navbar';
 
 function App() {
+  const [games, setGames] = useState([])
+
+  useEffect(() => {
+    fetch("http://localhost:3000/games")
+    .then((r) => r.json())
+    .then((data) => setGames(data))
+  }, [])
+
   return (
     <div className="App">
       <NavBar />
