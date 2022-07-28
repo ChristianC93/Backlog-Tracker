73ece39250103947dc2b960b3c815d0b34755e4d
Christian Castillo <castillochristian3@gmail.com>
2022-07-28 17:48:22 -0400

    passed games as prop to BacklogList and mapped over it. Passed individual game as prop to Item component

--- src/components/BacklogList.js
+++ src/components/BacklogList.js
@@ -5,7 +5,10 @@ import Item from "./Item";
 function BacklogList({games}) {
     return (
         <div>
-         
+            {games.map((game) => {
+                return (
+                    <Item key={game.id} game={game} />
+            )})}
         </div>
     )
 }
